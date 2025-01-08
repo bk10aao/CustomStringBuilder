268e75087acfe742775f78a733779be9c0b9aa60
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-01-08 15:06:49 +0000

    updating interface

--- src/main/java/StringBuilderInterface.java
+++ src/main/java/StringBuilderInterface.java
@@ -1,5 +1,17 @@
 public interface StringBuilderInterface {
 
-    String toString();
+    CustomStringBuilder append(boolean b);
+    CustomStringBuilder append(char c);
+    CustomStringBuilder append(char[] str);
+    CustomStringBuilder append(char[] str, int offset, int len);
+    CustomStringBuilder append(CharSequence charSequence);
+    CustomStringBuilder append(CharSequence charSequence, int start, int end);
+    CustomStringBuilder append(int i);
+    CustomStringBuilder append(long lng);
+    CustomStringBuilder append(float f);
+    CustomStringBuilder append(double d);
 
+    int length();
+
+    String toString();
 }
