6046b1965025a8161b241b6304d8abcd4ec7f487
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-01-08 12:55:58 +0000

    replaced char array with string list, updated toString() and tests

--- src/main/java/CustomStringBuilder.java
+++ src/main/java/CustomStringBuilder.java
@@ -1,57 +1,48 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomStringBuilder implements StringBuilderInterface {
 
-    private char[] stringCharacters = new char[127];
+    private List<String> stringBuilder = new ArrayList<>();
 
-    private int capacity = 0;
+    private int size = 0;
 
-    private int endIndex = 0;
-
-    public CustomStringBuilder() {
-        stringCharacters = new char[127];
-    }
+    public CustomStringBuilder() { }
 
     public CustomStringBuilder(CharSequence seq) {
-        if(seq != null)
-            buildString(seq.toString());
+        if(seq == null)
+            size = 0;
+        else {
+            size = seq.length();
+            stringBuilder.add(seq.toString());
+        }
     }
 
     public CustomStringBuilder(int capacity) {
         if(capacity < 0)
             throw new NegativeArraySizeException();
-        stringCharacters = new char[capacity];
-        this.capacity = capacity;
+        stringBuilder = new ArrayList<>(capacity);
     }
 
     public CustomStringBuilder(String str) {
-        if(str != null)
-            buildString(str);
+        if(str == null) {
+            size = 0;
+        } else {
+            size = str.length();
+            stringBuilder.add(str);
+        }
     }
 
+    @Override
     public String toString() {
-        if(endIndex == 0)
-            return "";
-        return new String(Arrays.copyOfRange(stringCharacters, 0, endIndex + 1));
+        String str = "";
+        for(String s : stringBuilder) {
+            str += s;
+        }
+        return str;
     }
 
-    private void buildString(String str) {
-        stringCharacters = str.length() * 2 < 128 ?  new char[127] : new char[str.length() * 2];
-        System.arraycopy(str.toCharArray(), 0, stringCharacters, 0, str.length());
-        this.capacity = str.length();
-        endIndex = str.isEmpty() ? 0 : str.length() - 1;
-    }
-
-    //TESTER METHODS
-    public int getStringCharactersSize() {
-        return stringCharacters.length;
-    }
-
-    public int getEndIndex() {
-        return endIndex;
-    }
-
-    public int getLength() {
-        return capacity;
+    public int length() {
+        return size;
     }
 }
--- src/test/java/StringBuilderTest.java
+++ src/test/java/StringBuilderTest.java
@@ -18,69 +18,43 @@ public class StringBuilderTest {
     }
 
     @Test
-    public void givenDefaultStringBuilder_returnsDefaultSizeOf_127_endIndexOf_0_andLengthOf_0() {
-        CustomStringBuilder customStringBuilder = new CustomStringBuilder();
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(0, customStringBuilder.getEndIndex());
-        assertEquals(0, customStringBuilder.getLength());
-    }
-
-    @Test
-    public void givenStringBuilder_withCharSequenceConstructor_of_null_returns_returnsDefaultSizeOf_127_endIndexOf_0_andLengthOf_0() {
+    public void givenStringBuilder_withCharSequenceConstructor_of_null_andLengthOf_0() {
         CharSequence charSequence = null;
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(charSequence);
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(0, customStringBuilder.getEndIndex());
-        assertEquals(0, customStringBuilder.getLength());
+        assertEquals(0, customStringBuilder.length());
     }
 
     @Test
-    public void givenStringBuilder_withCharSequenceConstructor_of_123abc_returns_returnsDefaultSizeOf_127_endIndexOf_0_andLengthOf_0() {
+    public void givenStringBuilder_withCharSequenceConstructor_of_123abc_returns_lengthOf_6() {
         CharSequence charSequence = "123abc";
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(charSequence);
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(5, customStringBuilder.getEndIndex());
-        assertEquals(6, customStringBuilder.getLength());
+        assertEquals(6, customStringBuilder.length());
     }
 
-    @Test
-    public void givenStringBuilder_withCharSequenceConstructorLargerThanDefaultSize_returns_returnsDefaultSizeOf_127_endIndexOf_0_andLengthOf_0() {
-        CustomStringBuilder customStringBuilder = new CustomStringBuilder(CHAR_SEQUENCE_VALUE_OF_LENGTH_129);
-        assertEquals(258, customStringBuilder.getStringCharactersSize());
-        assertEquals(128, customStringBuilder.getEndIndex());
-        assertEquals(129, customStringBuilder.getLength());
-    }
 
     @Test
-    public void givenStringBuilder_withStringConstructor_andEmptyString_returnsDefaultSizeOf_127_0_endIndexOf_0_andLengthOf_0() {
+    public void givenStringBuilder_withStringConstructor_andEmptyString_returns_lengthOf_0() {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder("");
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(0, customStringBuilder.getEndIndex());
-        assertEquals(0, customStringBuilder.getLength());
+        assertEquals(0, customStringBuilder.length());
+        assertEquals("", customStringBuilder.toString());
     }
 
     @Test
-    public void givenStringBuilder_withStringConstructor_null_returnsDefaultSizeOf_127_0_endIndexOf_0_andLengthOf_0() {
+    public void givenStringBuilder_withStringConstructor_null_returns_lengthOf_0() {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(null);
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(0, customStringBuilder.getEndIndex());
-        assertEquals(0, customStringBuilder.getLength());
+        assertEquals(0, customStringBuilder.length());
     }
 
     @Test
-    public void givenStringBuilder_withStringConstructor_andStringOf_123abc_returnsDefaultSizeOf_127_0_endIndexOf_5_andLengthOf_6() {
+    public void givenStringBuilder_withStringConstructor_andStringOf_123abc_lengthOf_6_andStringValue_123abc() {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder("123abc");
-        assertEquals(127, customStringBuilder.getStringCharactersSize());
-        assertEquals(5, customStringBuilder.getEndIndex());
-        assertEquals(6, customStringBuilder.getLength());
+        assertEquals(6, customStringBuilder.length());
     }
 
     @Test
-    public void givenStringBuilder_withStringConstructor_STRING_VALUE_OF_LENGTH_129_returnsDefaultSizeOf_258_endIndexOf_128_andLengthOf_129() {
+    public void givenStringBuilder_withStringConstructor_STRING_VALUE_OF_LENGTH_129_retunsLengthOf_129() {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(STRING_VALUE_OF_LENGTH_129);
-        assertEquals(258, customStringBuilder.getStringCharactersSize());
-        assertEquals(128, customStringBuilder.getEndIndex());
-        assertEquals(129, customStringBuilder.getLength());
+        assertEquals(129, customStringBuilder.length());
     }
 
     @Test
@@ -88,7 +62,6 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder();
         String stringValue = customStringBuilder.toString();
         assertEquals("", stringValue);
-        assertEquals(0, stringValue.length());
     }
 
     @Test
@@ -97,7 +70,6 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(charSequence);
         String stringValue = customStringBuilder.toString();
         assertEquals("", stringValue);
-        assertEquals(0, stringValue.length());
     }
 
     @Test
@@ -106,7 +78,6 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(charSequence);
         String stringValue = customStringBuilder.toString();
         assertEquals("123abc", stringValue);
-        assertEquals(6, stringValue.length());
     }
 
     @Test
@@ -114,7 +85,6 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder(127);
         String stringValue = customStringBuilder.toString();
         assertEquals("", stringValue);
-        assertEquals(0, stringValue.length());
     }
 
     @Test
@@ -122,7 +92,6 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder("");
         String stringValue = customStringBuilder.toString();
         assertEquals("", stringValue);
-        assertEquals(0, stringValue.length());
     }
 
     @Test
@@ -130,10 +99,8 @@ public class StringBuilderTest {
         CustomStringBuilder customStringBuilder = new CustomStringBuilder("123abc");
         String stringValue = customStringBuilder.toString();
         assertEquals("123abc", stringValue);
-        assertEquals(6, stringValue.length());
     }
 
     private static final String STRING_VALUE_OF_LENGTH_129 = "uwcwiavzhhigohtwixbrlxserzenalmzmkzwhrtewfzqpcvtsrnxkpdzcqsvpnqsatxjftfkhrdagqqunffpezghcpkuhlwrttdduhwgvpoqsksfojgtkgtkxkyzvbykl";
 
-    private static final CharSequence CHAR_SEQUENCE_VALUE_OF_LENGTH_129 = "uwcwiavzhhigohtwixbrlxserzenalmzmkzwhrtewfzqpcvtsrnxkpdzcqsvpnqsatxjftfkhrdagqqunffpezghcpkuhlwrttdduhwgvpoqsksfojgtkgtkxkyzvbykl";
 }
